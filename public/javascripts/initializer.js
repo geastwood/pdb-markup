0521e3e5098e89a3f321acfa8b7e7c2da6041444
Fei Liu <liu.feiwood@gmail.com>
2016-02-02 00:41:06 +0100

    add support for pdbConfig

--- public/javascripts/initializer.js
+++ public/javascripts/initializer.js
@@ -1,8 +1,23 @@
-PdbConnect = (() => {
+PdbConnect = ((global) => {
   return {
     init() {
       console.log('connector initialized');
       $('[data-toggle="tooltip"]').tooltip();
+      if (this.rewriteLink()) {
+        this.generateLink(global.PdbOnsiteConfig.rewriteLinkFn || (v => v));
+      }
+    },
+    hasConfig() {
+      return !!global.PdbOnsiteConfig;
+    },
+    rewriteLink() {
+      return this.hasConfig() && global.PdbOnsiteConfig.rewriteLink === true;
+    },
+    generateLink(f) {
+      $('.pdb-keyword-link').each((i, el) => {
+        var name = el.getAttribute('data-keyword-name');
+        el.setAttribute('href', f(name));
+      });
     }
   };
-})();
+})(this);
